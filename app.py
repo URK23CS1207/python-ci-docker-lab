bf47683711f58bebdb2165c8366bb9c97a87c3ba
Ms. Joann Of Arc A <URK23CS1207@KARUNYA.EDU>
2026-02-25 15:25:01 +0530

    add workflow file

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 def add(a, b):
-return a + b
-if __name__ ==
+    return a + b
+if __name__ == "__main__":
+    print("Hello from Python CI Lab!")
+    print("2 + 3 =", add(2, 3))
 
-"__main__":
-print("Hello from Python CI Lab!")
-print("2 + 3 =
-"
-, add(2, 3))
+
